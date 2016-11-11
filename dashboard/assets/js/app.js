e155e491d7a2ae06d6c4e9e6e3a094bb06503c6d
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-11-11 18:38:14 +0200

    add app module

---
+++ dashboard/assets/js/app.js
@@ -0,0 +1,39 @@
+var app = (function(){
+	var 
+		defaultPanel= '#panel-main'
+		,appPanel = '#wrapper-app'
+		,loaderPanel = '#wrapper-loader';
+
+	function trigger(evt, data){		
+		console.log(evt, data);
+		switch(evt){
+		}
+	}
+	
+	function init(){
+		_showDefaultPanel();
+		_eventListeners();
+		_hideLoader();
+		_showApp();
+	}
+
+	function _eventListeners(){
+	}
+	
+	function _showDefaultPanel(){
+		$(defaultPanel).show();
+	}
+
+	function _hideLoader(){
+		$(loaderPanel).hide();
+	}
+
+	function _showApp(){
+		$(appPanel).fadeIn('fast');
+	}
+
+	return {
+		trigger: trigger,
+		init: init
+	};
+})();
