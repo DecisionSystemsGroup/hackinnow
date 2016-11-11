d381fcc9ce1a8ebd6180bec00ec914a06f2ebbdf
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-11-11 21:57:35 +0200

    update charts

--- dashboard/assets/js/app.js
+++ dashboard/assets/js/app.js
@@ -3,7 +3,26 @@ var app = (function(){
 		defaultPanel= '#panel-main'
 		,appPanel = '#wrapper-app'
 		,loaderPanel = '#wrapper-loader'
-		,charts = [undefined, undefined, undefined];
+		,charts = [
+			{
+				type: 'line'
+				,wrapper: 'chart-0'
+				,colors: ['rgba(95, 156, 116 ,0.4)', '#5D6A64']
+				,obj: undefined
+			},
+			{
+				type: 'doughnut'
+				,wrapper: 'chart-1'
+				,colors: ['#8DB4A3', '#273C41']
+				,obj: undefined
+			},
+			{
+				type: 'radar'
+				,wrapper: 'chart-2'
+				,colors: ['#C2D2BD', '#5D6A64']
+				,obj: undefined
+			}
+		];
 
 	function trigger(evt, data){
 		console.log("Event triggered: "+evt, data);
@@ -35,27 +54,59 @@ var app = (function(){
 	}
 
 	function _createCharts(){
+		_createChart0();
 		_createChart1();
 		_createChart2();
-		_createChart3();
+	}
+
+	function _createChart0(){
+		var index = 0;
+		if(charts[index].obj !== undefined){
+			charts[index].obj.destroy();
+		}
+		var chartData = {
+		    labels: [65, 59, 80, 81, 56, 55, 40],
+		    datasets: [
+		        {
+		            label: "Iosif Mpoukas",
+					backgroundColor: charts[index].colors[0],
+					borderColor: charts[index].colors[1],
+					borderWidth: 1,
+		            data: [65, 59, 80, 81, 56, 55, 40]
+		        }
+		    ]
+    };
+		var ctx = document.getElementById(charts[index].wrapper);
+		charts[index] = new Chart(ctx,{
+			type: charts[index].type,
+			data: chartData,
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
+		});
 	}
 
 	function _createChart1(){
-		var index = 0;
-		if(charts[index] !== undefined){
-			charts[index].destroy();
+		var index = 1;
+		if(charts[index].obj !== undefined){
+			charts[index].obj.destroy();
 		}
 		var chartData = {
 			labels: ['Label1', 'Label2'],
 			datasets: [{
 				data: [78, 22],
-				backgroundColor:["#d5d5d5", "#FF6384"],
-				hoverBackgroundColor:["#36A2EB", "#FF6384"]
+				backgroundColor: charts[index].colors
 			}]
 		};
-		var ctx = document.getElementById('chart-1');
+		var ctx = document.getElementById(charts[index].wrapper);
 		charts[index] = new Chart(ctx,{
-			type: 'doughnut',
+			type: charts[index].type,
 			data: chartData,
 			options: {
 				title: {
@@ -73,53 +124,28 @@ var app = (function(){
 	}
 
 	function _createChart2(){
-		var index = 1;
-		if(charts[index] !== undefined){
-			charts[index].destroy();
+		var index = 2;
+		if(charts[index].obj !== undefined){
+			charts[index].obj.destroy();
 		}
 		var chartData = {
-			labels: ['Label1', 'Label2'],
-			datasets: [{
-				data: [55, 45],
-				backgroundColor:["#333", "#123654"],
-				hoverBackgroundColor:["#444", "#123654"]
-			}]
-		};
-		var ctx = document.getElementById('chart-2');
-		charts[index] = new Chart(ctx,{
-			type: 'doughnut',
-			data: chartData,
-			options: {
-				title: {
-					display: true,
-					fontSize: 25,
-					text: 'Some Title'
+			labels: ["Connections", "Stability", "Smoothness", "General score"],
+			datasets: [
+				{
+					label: 'Average User',
+					backgroundColor: charts[index].colors[0],
+					data: [49, 33, 44, 70]
 				},
-				animation: {
-					duration: 1500,
-					animateScale: true,
-					animateRotate: true
+				{
+					label: 'Target Values',
+					backgroundColor: charts[index].colors[1],
+					data: [59, 49, 155, 93]
 				}
-			}
-		});
-	}
-
-	function _createChart3(){
-		var index = 3;
-		if(charts[index] !== undefined){
-			charts[index].destroy();
-		}
-		var chartData = {
-			labels: ['Label1', 'Label2'],
-			datasets: [{
-				data: [33, 125],
-				backgroundColor:["#d72f11", "#123aaa"],
-				hoverBackgroundColor:["#d62e11", "#123aaa"]
-			}]
+			]
 		};
-		var ctx = document.getElementById('chart-3');
+		var ctx = document.getElementById(charts[index].wrapper);
 		charts[index] = new Chart(ctx,{
-			type: 'doughnut',
+			type: charts[index].type,
 			data: chartData,
 			options: {
 				title: {
